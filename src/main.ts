d4c470ed501f9f5251d8a3ac07ffe5a6dfc92b5e
John Christiansen <johnchristianen@gmail.com>
2024-09-26 21:16:46 -0600

    updates

--- src/main.ts
+++ src/main.ts
@@ -142,12 +142,12 @@ export const loop = ErrorMapper.wrapLoop(() => {
     var creep = Game.creeps[name];
 
 
-    if(Game.flags.reallocateFlag && creep.memory.firstSpawnCoords === "W3N9" && creep.room.name !== Game.flags.reallocateFlag.room?.name) {
-      console.log('reallocating...')
-      creep.say("↔")
-      creep.moveTo(Game.flags.reallocateFlag);
-      return;
-    }
+    // if(Game.flags.reallocateFlag && creep.memory.firstSpawnCoords === "W3N9" && creep.room.name !== Game.flags.reallocateFlag.room?.name) {
+    //   console.log('reallocating...')
+    //   creep.say("↔")
+    //   creep.moveTo(Game.flags.reallocateFlag);
+    //   return;
+    // }
 
     if(creep.memory?.isArmySquad && Memory?.economyType && Game.flags.rallyFlag?.pos &&  creep?.pos &&  Game.flags.stagingFlag &&  !creep.pos.inRangeTo(Game.flags.stagingFlag.pos.x,Game.flags.stagingFlag.pos.y,6)) {
       const totalArmySize =  _.filter(Game.creeps, (creep) => creep.memory.isArmySquad )?.length ?? 0;
