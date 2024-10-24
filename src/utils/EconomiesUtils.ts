9e666e5b7c7748be40bf1ece556682e54ce33423
John Christiansen <johnchristianen@gmail.com>
2024-10-23 18:32:41 -0600

    updates

--- src/utils/EconomiesUtils.ts
+++ src/utils/EconomiesUtils.ts
@@ -60,11 +60,11 @@ export enum WarTimeEconomy {
 
 export enum PeaceTimeEconomy {
     //ARMY Economy
-    TOTAL_ATTACKER_SIZE =4,
-    TOTAL_HEALER_SIZE =1,
+    TOTAL_ATTACKER_SIZE =2,
+    TOTAL_HEALER_SIZE =0,
     TOTAL_DISMANTLER_SIZE = 0,
     TOTAL_MEAT_GRINDERS = 0,
-    TOTAL_SCOUT_SIZE = 3
+    TOTAL_SCOUT_SIZE = 1
 
 }
 
