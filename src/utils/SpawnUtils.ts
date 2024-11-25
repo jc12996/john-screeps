453fb8523d843873677392ad012c870965c8a398
John Christiansen <john.christiansen@limblecmms.com>
2024-11-24 17:39:13 -0700

    updates

--- src/utils/SpawnUtils.ts
+++ src/utils/SpawnUtils.ts
@@ -103,11 +103,11 @@ export class SpawnUtils {
                 }
                 else if(!hauling && commandLevel < 6  && lowerMiner && energyAvailable >= 500) {
 
-                    for (let i = 0; i < 3; i++) {
+                    for (let i = 0; i < 1; i++) {
                         partsPattern.push(MOVE);
                     }
 
-                    for (let i = 0; i < 3; i++) {
+                    for (let i = 0; i < 4; i++) {
                         partsPattern.push(WORK);
                     }
                     for (let i = 0; i < 1; i++) {
