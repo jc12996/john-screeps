b8182681c0b3ca18f9206bd23beedf56c41f703f
John Christiansen <johnchristianen@gmail.com>
2024-12-27 23:44:13 -0700

    ff

--- src/roles/builder.ts
+++ src/roles/builder.ts
@@ -39,7 +39,7 @@ export class Builder {
         creep.memory.building = false;
         creep.say("🔄 harvest");
       }
-      if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+      if (!creep.memory.building && (creep.store.getFreeCapacity() == 0 || creep.store[RESOURCE_ENERGY] >  0)) {
         creep.memory.building = true;
         creep.say("🔨 build");
       }
