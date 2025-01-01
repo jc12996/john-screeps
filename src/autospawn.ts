86dd2dd23a92d8a93f7f00709f9b867064c30d8b
John Christiansen <johnchristianen@gmail.com>
2025-01-01 12:15:27 -0700

    dd

--- src/autospawn.ts
+++ src/autospawn.ts
@@ -275,7 +275,7 @@ export class AutoSpawn {
 
         // Process each mineFlag as needed
         // Example: Adjust number of needed miners and haulers based on each mineFlag
-        let numberOfNeededMiners = numberOfSourcesInMineFlagRoom >= 2 ? 3 : 2;
+        let numberOfNeededMiners = numberOfSourcesInMineFlagRoom >= 2 ? 3 : 1;
         let numberOfNeededHaulers = numberOfSourcesInMineFlagRoom >= 2 ? 4 : 2;
         let numberOfNeededAttackClaimers = LowUpkeep.AttackClaimers * 1;
 
